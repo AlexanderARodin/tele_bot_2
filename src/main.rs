7000a9fb9852de92728f453da9bd9c97bd26190f
Alexander A.Rodin <@inbox.ru>
2024-08-07 00:08:52 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use teloxide::{prelude::*, RequestError};
+
+#[tokio::main]
+async fn main() {
+    println!("Initializing..");
+    let token = &env::args().nth(1).expect("E: Token must be privided as an argument");
+    {
+        let bot = Bot::new(token);
+        println!("started!..");
+
+        teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+            repl_handler(&bot, &msg)
+                .await
+        })
+            .await;
+    }
+}
+
+async fn repl_handler( bot: &Bot, msg: &Message ) -> Result<(), RequestError> {
+    let msg_text = msg.text();
+    let chat_id = msg.chat.id;
+    println!("\nID: {}\nMessage: {:?}\n",chat_id, msg_text);
+    bot.send_message(chat_id, "<--->")
+        .await?;
+    Ok(())
 }
